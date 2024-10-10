f1ec1810a10f266a1e565a4d15609d314d03f691
idoz1 <169044973+idoz1@users.noreply.github.com>
2024-10-10 21:56:28 +0300

    added fetch functionality and job info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,43 @@
+import { useEffect, useState } from "react";
+import JobInfo from "./components/JobInfo";
+
 const url = 'https://www.course-api.com/react-tabs-project';
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [isLoading, setIsLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+
+  const fetchJobs = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const newJobs = await response.json();
+      setJobs(newJobs);
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+    setIsLoading(false);
+  }
+  
+
+  useEffect(() => {
+    fetchJobs();
+  }, [])
+
+  if(isLoading){
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    )
+  }
+
+  return (
+    <section className="jobs-center">
+      <JobInfo jobs={jobs} />
+    </section>
+  );
 };
 export default App;
